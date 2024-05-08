e6f9ed5a828ec777eeaba40428aaf7b9ee599bf8
Arka Barua <arka.barua@northsouth.edu>
2024-05-09 03:02:22 +0600

    Skills and Socials added

---
+++ src/components/Skill.js
@@ -0,0 +1,56 @@
+export default function Skill() {
+  const language_items = [
+    { text: "Python" },
+    { text: "SQL" },
+    { text: "JavaScript" },
+  ];
+
+  const package_items = [
+    { text: "Pandas" },
+    { text: "PostgreSQL" },
+    { text: "Scikit-learn" },
+    { text: "React" },
+  ];
+
+  const viz_items = [
+    { text: "Matplotlib" },
+    { text: "Seaborn" },
+    { text: "Plotly" },
+    { text: "PowerBI" },
+  ];
+
+  return (
+    <div className="py-4 mt-10 text-gray-50">
+      <div className="pb-4 text-2xl tracking-wide font-bold">Skills</div>
+      <h3 className="text-base font-semibold">💻 Programming Languages</h3>
+      <ul className="flex">
+        {language_items.map((item, index) => (
+          <li key={index} className="pr-4 py-4">
+            <span className="text-green-700 font-extrabold mr-2">&#10003;</span>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      <h3 className="text-base font-semibold">📚 Libraries</h3>
+      <ul className="flex">
+        {package_items.map((item, index) => (
+          <li key={index} className="pr-4 py-4">
+            <span className="text-green-700 font-extrabold mr-2">&#10003;</span>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      <h3 className="text-base font-semibold">📊 Visualization Tools</h3>
+      <ul className="flex">
+        {viz_items.map((item, index) => (
+          <li key={index} className="pr-4 py-4">
+            <span className="text-green-700 font-extrabold mr-2">&#10003;</span>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Social.js
@@ -0,0 +1,40 @@
+import github from "../assets/GitHub_Logo_White.png";
+import kaggle from "../assets/kaggle-logo-transparent-300.png";
+import LinkedIn from "../assets/LinkedIn-Blue-40@2x.png";
+
+export default function SocialLinks() {
+  return (
+    <div className="text-gray-50">
+      <h2 className="font-bold text-2xl mb-10">🖇️ Profile Links</h2>
+      <ul className="flex w-64 mx-auto align-middle justify-between">
+        <li>
+          <a href="https://github.com/ArkaBarua173">
+            <img
+              src={github}
+              className="w-auto h-6 object-cover"
+              alt="github_logo"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.kaggle.com/arkabarua173">
+            <img
+              src={kaggle}
+              className="w-[60px] h-6 object-cover"
+              alt="kaggle_logo"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/arka-barua/">
+            <img
+              src={LinkedIn}
+              className="w-[80px] h-5 object-cover"
+              alt="linkedin_logo"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
